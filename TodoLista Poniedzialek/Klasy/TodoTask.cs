b33fa42699b45fa0071e69cc5e089f653439f00b
szymonworonowicz <49445507+szymonworonowicz@users.noreply.github.com>
2021-12-20 18:58:29 +0100

    part

---
+++ TodoLista Poniedzialek/Klasy/TodoTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoLista_Poniedzialek.Klasy
+{
+	public class TodoTask
+	{
+		public int Id;
+		public Guid UserId;
+		public string Tytul;
+		public string Opis;
+		public bool Zrobione;
+
+		public TodoTask(int id, Guid userId, string tytul, string opis, bool zrobione)
+		{
+			Id = id;
+			UserId = userId;
+			Tytul = tytul;
+			Opis = opis;
+			Zrobione = zrobione;
+		}
+	}
+}
---
+++ TodoLista Poniedzialek/Klasy/TodoTaskManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoLista_Poniedzialek.Klasy
+{
+    //klasa do zarzadzania zadaniami
+    public class TodoTaskManager
+    {
+        //lista zadan
+        private List<TodoTask> zadania;
+
+        public TodoTaskManager()
+        {
+            zadania = new List<TodoTask>();
+        }
+
+        // metoda zwracająca zadanie znalezione po Id
+        public TodoTask ZnajdzZadanie(int id)
+        {
+            //iterujemy po wszystkich zadaniach
+            foreach (TodoTask task in zadania)
+            {
+                // jak znalezione zadanie ma id takie jak przekazane
+                // to je zwracamy
+                if (task.Id == id)
+                {
+                    return task;
+                }
+            }
+            // jeżeli nie ma zadania o podanym id to zwraqcamy nulla
+            return null;
+        }
+
+        //dodawanie zadania na liste
+        public void DodajZadanie(TodoTask zadanie)
+        {
+            zadania.Add(zadanie);
+        }
+        //usuniecie zadania z listy
+        public void UsunZadanie(int id)
+        {
+            TodoTask zadanie = ZnajdzZadanie(id);
+            zadania.Remove(zadanie);
+        }
+    }
+}
